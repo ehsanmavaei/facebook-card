526657f6f3216b7ed7e2dbd8a34c3acaf939195f
ehsanmavaei <ehsanmavaei@gmail.com>
2022-04-09 15:41:22 +0430

    add functionality to the card

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,22 @@ console.log('HELLO');
 const test = () => {
 	console.log('this is a test');
 };
+
+var controlsContainer = document.getElementById('controls-container'),
+    allImages = document.querySelectorAll('.image'),
+    imagesContainer = document.getElementById('images-container');
+
+
+controlsContainer.onclick = function(e){
+  var target = e.target,
+      dataTarget = target.getAttribute('data-image'),      
+      activeImage = document.getElementById(dataTarget);
+  
+  if(dataTarget){
+    for( var i = 0; i < allImages.length; i++){
+    allImages[i].removeAttribute('data-active');
+  }
+ 
+  activeImage.setAttribute('data-active', 'active');
+  }  
+}
